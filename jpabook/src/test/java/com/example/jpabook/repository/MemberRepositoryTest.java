7ad3df8450bb5d71a6581844dbc023cc9ff99891
gummybearr <ehrudghl123@naver.com>
2021-01-25 02:45:01 +0900

    update study materials

---
+++ jpabook/src/test/java/com/example/jpabook/repository/MemberRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.jpabook.repository;
+
+import com.example.jpabook.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional()
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+//        System.out.println(findMember);
+//        System.out.println(saveId);
+        assertThat(findMember.getId()).isSameAs(saveId);
+    }
+}
